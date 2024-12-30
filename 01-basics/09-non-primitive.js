8a7f87380795268e470df2f0dd817def4e87a79f
Jiya Jain <jiyajain.dev@gmail.com>
2024-12-30 09:39:42 +0000

    conditional

--- 01-basics/09-non-primitive.js
+++ 01-basics/09-non-primitive.js
@@ -4,8 +4,19 @@ let username ={}
 console.log(username)
 console.log(typeof username)
 
-let username1 ={ name1:jiya,
+let username1 = { "name1" : "jiya" ,
     isloggedin: true 
 } //more easy and good way to write 
 
-// Testing git change
+//array
+let anotherusername = ["jiya",true]
+console.log(anotherusername[0])
+
+ //type conversion 
+ //explicit
+let isvalue = true //javascript consider true as 1 and false as 0
+ console.log(isvalue + 1); //this will give us 2, this is done by lang java 
+//implicit
+console.log(number(isvalue) + 1)// will change isvalue to number this is done by user
+console.log(number(null))//will convert null to 0
+console.log(number(undefined))//will convert undefined to 
