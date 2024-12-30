91cdc775eba2684cbc8b633f0de82167db9c7813
Jiya Jain <jiyajain.dev@gmail.com>
2024-12-30 11:45:08 +0000

    condition

--- 01-basics/09-non-primitive.js
+++ 01-basics/09-non-primitive.js
@@ -17,6 +17,6 @@ console.log(anotherusername[0])
 let isvalue = true //javascript consider true as 1 and false as 0
  console.log(isvalue + 1); //this will give us 2, this is done by lang java 
 //implicit
-console.log(number(isvalue) + 1)// will change isvalue to number this is done by user
-console.log(number(null))//will convert null to 0
-console.log(number(undefined))//will convert undefined to 
+console.log(Number(isvalue) + 1)// will change isvalue to number this is done by user
+console.log(Number(null))//will convert null to 0
+console.log(Number(undefined))//will convert undefined to 
---
+++ 01-basics/10-conditional-statements.js
@@ -0,0 +1,28 @@
+//check if one number is greater than another
+let num1 =  5;
+let num2 =  8;
+if  (num1<num2) {
+    console.log("num1 is greater than num2")
+} else {
+    "nope,you are wrong stupid , how can 5 can be less than 8????"
+}
+console.log("this statement will work anyways")
+
+//checking if one string is equal to another string
+let username = "chai"
+let anotherusername ="chai"
+if  (username=anotherusername) {    
+    console.log("pick another username which is unique"
+    )
+} else if (username != anotherusername) {
+    console.log("good  username, you can have it")
+} else {
+    "error"
+}
+//checking if an array is empty or not
+let list = [];
+if (list.length==0) { // length is function that gives u how many elemets are present in the array
+    console.log("array is empty")
+} else{
+    "this is not an empty array"
+}
